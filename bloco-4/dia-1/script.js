c2a9d84a8b888609c471458a1c6b9e224cf96c32
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2021-06-23 18:51:32 -0300

    exercicio 2 Comparando dois numero

--- bloco-4/dia-1/script.js
+++ bloco-4/dia-1/script.js
@@ -17,3 +17,15 @@ console.log(a/b);
 
 //Modulo
 console.log(a%b);
+
+// Exercicio 2
+
+let num1 = 22;
+let num2 = 12;
+let comparacao;
+
+if(num1 > num2){
+  console.log("O num1 é maior que o num2");
+} else {
+  console.log("O numero num1 é menor que o num2");
+}
