21835fa4dfb5cb3907ff48ffa4fb6e4a12c31d24
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2021-06-23 18:42:00 -0300

    Renomeamos o arquivo para cada exercicio

---
+++ bloco-4/dia-1/exercicio1.js
@@ -0,0 +1,19 @@
+let a = 3;
+let b = 7;
+
+// Adição
+console.log(a + b);
+
+//Subtração
+console.log(a -b);
+
+// Multiplicação
+console.log(a*b);
+
+//Divisão
+a = 10;
+b = 2;
+console.log(a/b);
+
+//Modulo
+console.log(a%b);
