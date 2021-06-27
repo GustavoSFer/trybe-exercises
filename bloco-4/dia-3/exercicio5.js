8baa466e2b5827082c855da14b0afd1133887032
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2021-06-27 14:24:54 -0300

    salvando

--- bloco-4/dia-3/exercicio5.js
+++ bloco-4/dia-3/exercicio5.js
@@ -3,6 +3,7 @@ let middle = (n + 1) / 2;
 let controlLeft = middle;
 let controlRight = middle;
 let symbol = '*';
+
 for (let line = 1; line <= middle; line += 1) {
   let outputLine = '';
   for (let col = 1; col <= n; col += 1) {
