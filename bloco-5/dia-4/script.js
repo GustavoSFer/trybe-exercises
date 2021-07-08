43c6c8f4c93792742b93bcb3b776a51c1fb8fce4
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2021-07-08 09:09:09 -0300

    mudando com de fundo do nosso body

---
+++ bloco-5/dia-4/script.js
@@ -0,0 +1,20 @@
+const pega = document.getElementById('mudaCor');
+pega.addEventListener("click", function(evento){
+  console.log(evento.target)
+});
+
+let botao = document.querySelector('#mudaCor');
+let criaBotao = document.createElement('button');
+criaBotao.innerText = "Mudar de cor";
+botao.appendChild(criaBotao);
+
+criaBotao.addEventListener("click", function(){
+  let corFundo = document.querySelector('body');
+  if(corFundo.style.backgroundColor === 'blue') {
+    corFundo.style.backgroundColor = 'white';
+    console.log('branco');
+  } else {
+    corFundo.style.backgroundColor = 'blue';
+    console.log('azul');
+  }
+})
