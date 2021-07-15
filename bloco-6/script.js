3fc982bf20cf11c7fc9c0665e74162e142d70237
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2021-07-15 14:54:20 -0300

    criando estado via js

---
+++ bloco-6/script.js
@@ -0,0 +1,13 @@
+let estado = ['São paulo', 'Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia','Ceará' , 'Distrito Federal' , 'Espírito Santo'];
+
+let getEstado = document.getElementById('estado');
+
+for (let index = 0; index < estado.length; index++) {
+  let listEstado = document.createElement('option');
+  teste = estado[index];
+  listEstado.value = teste;
+  listEstado.innerText = teste;
+
+  getEstado.appendChild(listEstado);
+  
+}
