acc73a0621bca2da54102f4ce43ccb7b074f9920
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2021-07-15 15:26:04 -0300

    copiando arquivos do dia anterior para continuar os exercicios

---
+++ bloco-6/dia-2/script.js
@@ -0,0 +1,18 @@
+let estado = ['São paulo', 'Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia','Ceará' , 'Distrito Federal' , 'Espírito Santo'];
+
+let getEstado = document.getElementById('estado');
+
+for (let index = 0; index < estado.length; index++) {
+  let listEstado = document.createElement('option');
+  teste = estado[index];
+  listEstado.value = teste;
+  listEstado.innerText = teste;
+
+  getEstado.appendChild(listEstado);
+  
+}
+
+let enviar = document.getElementById('enviar');
+enviar.addEventListener('click', function(event){
+  event.preventDefault();
+});
