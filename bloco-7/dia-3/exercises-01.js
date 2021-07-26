89b6c1932071b16819dd284766eaddc2a2b7ee4e
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2021-07-26 19:17:41 -0300

    arrumando os exercicios 1

--- bloco-7/dia-3/exercises-01.js
+++ bloco-7/dia-3/exercises-01.js
@@ -10,12 +10,11 @@ function sum(a, b) {
 
 const expected = sum(4, 5);
 // (variavel criada que esta com a nossa função, valor esperado de retorno, 'msagem de erro')
-assert.equal(expected, 9, 'Error, Verifique seu código.')
+assert.strictEqual(sum(4, 5), 9)
 
 //retorno esperado 0
 const expected0 = sum(0, 0);
-assert.equal (expected0, 0, 'Error, verifique seu código')
-
+assert.strictEqual(sum(0, 0), 0)
 /*Teste se a função sum lança um erro quando os parâmetros são 4 e "5" (string 5)
 Teste se a mensagem de erro é "parameters must be numbers" quando realizar a chamada sum(4, "5")
 */
