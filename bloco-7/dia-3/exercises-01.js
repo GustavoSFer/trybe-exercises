8fb84864c2025a13a348762f91bceb0a84456825
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2021-07-26 16:06:07 -0300

    verificando se gera erro ao passar string

--- bloco-7/dia-3/exercises-01.js
+++ bloco-7/dia-3/exercises-01.js
@@ -12,5 +12,13 @@ const expected = sum(4, 5);
 // (variavel criada que esta com a nossa função, valor esperado de retorno, 'msagem de erro')
 assert.equal(expected, 9, 'Error, Verifique seu código.')
 
+//retorno esperado 0
 const expected0 = sum(0, 0);
 assert.equal (expected0, 0, 'Error, verifique seu código')
+
+/*Teste se a função sum lança um erro quando os parâmetros são 4 e "5" (string 5)
+Teste se a mensagem de erro é "parameters must be numbers" quando realizar a chamada sum(4, "5")
+*/
+const expectedString = sum(4, '5');
+assert.equal(expectedString, 9, 'Error, Verifique seu código.')
+// Retorno: Sim, gera o erro e a mensagem.
