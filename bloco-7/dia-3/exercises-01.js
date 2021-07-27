1eaf3bd4ed44308d96611b6667d9aade955e2077
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2021-07-26 21:10:09 -0300

    arrumando o exercicio 4

--- bloco-7/dia-3/exercises-01.js
+++ bloco-7/dia-3/exercises-01.js
@@ -8,16 +8,22 @@ function sum(a, b) {
   return a + b;
 }
 
-const expected = sum(4, 5);
+
 // (variavel criada que esta com a nossa função, valor esperado de retorno, 'msagem de erro')
-assert.strictEqual(sum(4, 5), 9)
+assert.strictEqual(sum(4, 5), 9);
 
 //retorno esperado 0
-const expected0 = sum(0, 0);
-assert.strictEqual(sum(0, 0), 0)
+assert.strictEqual(sum(0, 0), 0);
+
 /*Teste se a função sum lança um erro quando os parâmetros são 4 e "5" (string 5)
 Teste se a mensagem de erro é "parameters must be numbers" quando realizar a chamada sum(4, "5")
 */
-const expectedString = sum(4, '5');
-assert.equal(expectedString, 9, 'Error, Verifique seu código.')
-// Retorno: Sim, gera o erro e a mensagem.
+
+//4 Espera que o bloco lance um erro. o erro pode ser construtor, RegExp ou função de validação.
+//O assert.throws permite verificar erros específicos em suas funções.
+//estamos passando uma função sem nome e dentro dela a função sum e esta nos retornando um erro
+// com isso esta correto.
+assert.throws(() => {
+  sum(4, '5');
+});
+
