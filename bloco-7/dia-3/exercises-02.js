8f71356a05583b86b53f79894a09c8aa4d17303b
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2021-07-26 21:31:06 -0300

    exercicio 2

--- bloco-7/dia-3/exercises-02.js
+++ bloco-7/dia-3/exercises-02.js
@@ -11,5 +11,8 @@ function myRemove(arr, item) {
   return newArr;
 }
 
-//1 
+//1 deepStrictEqual - Se dois objetos, ou seus objetos filhos, não forem iguais (tanto em valor quanto em tipo), um erro é lançado e o programa é encerrado.
 assert.deepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 4]);
+
+//2
+assert.notDeepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 3, 4]);
