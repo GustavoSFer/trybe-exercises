3cd9ddac55688ce6525705c552a5bdbe6e3dca39
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2021-07-28 19:09:38 -0300

    arrumando o exercicio 1

--- bloco-8/dia-1/exercises01.js
+++ bloco-8/dia-1/exercises01.js
@@ -1,17 +1,20 @@
-let pessoa = (nome) => {
-  return {
-    nomeCompleto: nome,
-    email: `${nome}@trybe.com`
-  }
-}
-
 const newEmployees = (nome) => {
   const employees = {
-    id1: pessoa(nome), 
-    id2: '', // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
-    id3: '', // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+    id1: nome('Pedro Guerra'),
+    // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: nome('Luiza Drumond'), 
+    // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: nome('Carla Paiva'), 
+    // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
   }
   return employees;
+};
+
+const nome = (nome) => {
+  const geraEmai = {
+    email: nome.toLowerCase().split(' ').join('_')
+  }
+  return `nome: ${nome} email:${geraEmai.email}@trybe.com`;
 }
 
-console.log(newEmployees('Gustavo Fernandes'));
+console.log(newEmployees(nome))
