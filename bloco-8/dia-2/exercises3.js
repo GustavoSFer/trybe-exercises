d9110a4a5e3a60f045551a33ee67337c99b508cf
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2021-07-29 21:38:57 -0300

    exercicio 2

---
+++ bloco-8/dia-2/exercises3.js
@@ -0,0 +1,21 @@
+let teste =[
+  {
+    name: 'Gustavo',
+    idade: 23,
+    outro: {
+      rua: 'fidsoifsd'
+    }
+  },
+  {
+    name: 'Joao',
+    idade: 43,
+    outro: {
+      rua: 'fjids fjdsiojfs'
+    }
+  }
+]
+   
+
+let meuNome = teste.name;
+console.log(teste[0].name)
+console.log(meuNome)
