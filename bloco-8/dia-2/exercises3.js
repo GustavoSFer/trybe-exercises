712fa59cb74d7163e45efe3f32328bfb995f43db
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2021-07-29 21:45:49 -0300

    exercicio 3

--- bloco-8/dia-2/exercises3.js
+++ bloco-8/dia-2/exercises3.js
@@ -1,21 +1,83 @@
-let teste =[
+const assert = require('assert');
+
+const books = [
   {
-    name: 'Gustavo',
-    idade: 23,
-    outro: {
-      rua: 'fidsoifsd'
-    }
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: {
+      name: 'George R. R. Martin',
+      birthYear: 1948,
+    },
+    releaseYear: 1991,
   },
   {
-    name: 'Joao',
-    idade: 43,
-    outro: {
-      rua: 'fjids fjdsiojfs'
-    }
-  }
-]
-   
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: {
+      name: 'J. R. R. Tolkien',
+      birthYear: 1892,
+    },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Isaac Asimov',
+      birthYear: 1920,
+    },
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Frank Herbert',
+      birthYear: 1920,
+    },
+    releaseYear: 1965,
+  },
+  {
+    id: 5,
+    name: 'A Coisa',
+    genre: 'Terror',
+    author: {
+      name: 'Stephen King',
+      birthYear: 1947,
+    },
+    releaseYear: 1986,
+  },
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: {
+      name: 'H. P. Lovecraft',
+      birthYear: 1890,
+    },
+    releaseYear: 1928,
+  },
+];
 
-let meuNome = teste.name;
-console.log(teste[0].name)
-console.log(meuNome)
+// Adicione o código do exercício aqui:
+const expectedResult = {
+  author: {
+    birthYear: 1948,
+    name: 'George R. R. Martin',
+  },
+  genre: 'Fantasia',
+  id: 1,
+  name: 'As Crônicas de Gelo e Fogo',
+  releaseYear: 1991,
+};
+
+function getNamedBook() {
+  // escreva seu código aqui
+  return books.find((value) => value.name.length === 26)
+}
+
+assert.deepStrictEqual(getNamedBook(), expectedResult);
