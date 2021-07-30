0a936f735ae9ecd8d0d7bd4765b52896ddc46bf8
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2021-07-30 19:16:58 -0300

    requisito numero 1

---
+++ bloco-8/dia-3/teste.js
@@ -0,0 +1,15 @@
+let nome = [
+  {
+    name: 'Gustavo',
+    idade: 23,
+  }
+]
+
+console.log(nome[0].name);
+
+let outroNome = {
+  name: 'gustavo',
+  idade: 43,
+}
+
+console.log(outroNome.name)
