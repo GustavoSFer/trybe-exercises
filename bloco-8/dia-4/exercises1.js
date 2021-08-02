74d046c31dc2fcf55c38bdee41429a1fffa4ae76
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2021-08-02 18:38:39 -0300

    refazendo o exercicio um para entendimento melhor

--- bloco-8/dia-4/exercises1.js
+++ bloco-8/dia-4/exercises1.js
@@ -1,5 +1,22 @@
 const assert = require('assert');
 
+// const arrays = [
+//   ['1', '2', '3'],
+//   [true],
+//   [4, 5, 6],
+// ];
+
+// function flatten() {
+//   // escreva seu código aqui
+//   return arrays.reduce((acc, value) => acc.concat(value), []);
+// }
+//  //console.log(flatten());
+//  assert.deepStrictEqual(flatten(), ['1', '2', '3', true, 4, 5, 6]);
+
+
+
+//  const assert = require('assert');
+
 const arrays = [
   ['1', '2', '3'],
   [true],
@@ -8,7 +25,8 @@ const arrays = [
 
 function flatten() {
   // escreva seu código aqui
-  return arrays.reduce((acc, value) => acc.concat(value), []);
+  return arrays.reduce((acc, arr) => acc.concat(arr), []);
 }
- //console.log(flatten());
- assert.deepStrictEqual(flatten(), ['1', '2', '3', true, 4, 5, 6]);
+
+console.log(flatten())
+assert.deepStrictEqual(flatten(), ['1', '2', '3', true, 4, 5, 6]);
