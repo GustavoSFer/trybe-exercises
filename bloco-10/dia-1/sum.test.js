d55b58b4be7c67b1a91b306326d24003406fd933
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2021-08-19 21:03:08 -0300

    exercicio 1 realizado

--- bloco-10/dia-1/sum.test.js
+++ bloco-10/dia-1/sum.test.js
@@ -12,11 +12,9 @@ describe ('Realizando teste na funcção sum', () => {
   });
 
   test('Teste se a função sum lança um erro quando os parâmetros são 4 e "5" (string 5)', ()=> {
-    try {
-      sum(4, '5');
-    } catch(error) {
-      expect(error.message).toBe('parameters must be numbers');
-    }
+   expect(()=> {
+     sum(4, '5').toThrow(/parameters must be numbers/);
+   })
   });
 
   test('Teste se a mensagem de erro é "parameters must be numbers" quando realizar a chamada sum(4, "5")', () => {
