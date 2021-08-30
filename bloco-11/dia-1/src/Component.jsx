42d6098f9af744e1b9c980bf26d798f2a5cb9848
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2021-08-30 18:17:25 -0300

    meu primeito teste com React

---
+++ bloco-11/dia-1/src/Component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class HelloWorld extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Gustavo Fernandes</h1>
+        <p>Meu primeiro teste/programa com o React</p>
+      </div>
+    );
+  }
+}
+
+export default HelloWorld;
