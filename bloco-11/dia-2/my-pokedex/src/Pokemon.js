c7536524382636f3a3cc57b5d41c43c332bf0441
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2021-09-01 13:30:22 -0300

    colocando classes css para estilizar

--- bloco-11/dia-2/my-pokedex/src/Pokemon.js
+++ bloco-11/dia-2/my-pokedex/src/Pokemon.js
@@ -1,20 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import App from './index.css'
 
 class Pokemon extends React.Component {
   render() {
     const { pk } = this.props;
 
     return (
-      <div>
-       <h2>{pk.name}</h2>
-       <p>{pk.type}</p>
-       <p>{pk.averageWeight.value} {pk.averageWeight.measurementUnit}</p>
-       <img src={pk.image} alt={pk.name}/>
+      <div className='card-pokemon'>
+        <div className='info-pokemon'>
+          <h2>{pk.name}</h2>
+          <p>{pk.type}</p>
+          <p>{pk.averageWeight.value} {pk.averageWeight.measurementUnit}</p>
+        </div>
+        <img src={pk.image} alt={pk.name}/>
       </div>
     )
   }
 }
 
-
+Pokemon.propTypes = {
+  name: PropTypes.string,
+  type: PropTypes.string.isRequired,
+}
 
 export default Pokemon;
