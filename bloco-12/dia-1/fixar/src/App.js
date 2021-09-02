962a65862861350f7e750ecf5734324dddd425ab
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2021-09-02 18:56:30 -0300

    exercicio de fixao

--- bloco-12/dia-1/fixar/src/App.js
+++ bloco-12/dia-1/fixar/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+    this.click = this.click.bind(this);
+  }
+  click() {
+    console.log('clicado!')
+    console.log(this);
+  }
+  render() {
+    return(
+      <button type="button" onClick={this.click}>Clique Aqui!</button>
+    );
+  }
 }
 
 export default App;
