ce469820abbdbae6ea21864b1337a1c9ff1c028b
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2021-09-05 17:41:46 -0300

    criando 3 botoes

--- bloco-12/dia-1/fixar/src/App.js
+++ bloco-12/dia-1/fixar/src/App.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import './App.css';
+import Button from './Components/Button';
 
-function click() {
+function click1() {
   console.log("Clicou no botão!")
+}
+function onMouseOver() {
+  console.log("Passou o mou em cima!")
+}
+function onMouseOut() {
+  console.log("Tirou o mouse de cima!")
 }
 
 class App extends React.Component {
   render() {
     return (
-      <button onClick={ click }>Meu botão</button>
+      <div>
+        <button onClick={click1} >Botao 1</button>
+        <button onMouseOver={onMouseOver} >Botao 2</button>
+        <button onMouseOut={onMouseOut} >Botao 3</button>
+
+      </div>
     );
+     
   }
 
 }
