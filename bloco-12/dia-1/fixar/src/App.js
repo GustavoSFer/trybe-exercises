fc8bfb0b8543cfc71fdb97950526f543f059fd9b
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2021-09-05 19:04:19 -0300

    realizando a contagem dentro do state

--- bloco-12/dia-1/fixar/src/App.js
+++ bloco-12/dia-1/fixar/src/App.js
@@ -4,25 +4,45 @@ import './App.css';
 class App extends React.Component {
   constructor() {
     super()
-    this.click1 = this.click1(this);
+
+    this.state = {
+      numeroClick: 0,
+      mouseDentro: 0,
+      mouseFora: 0,
+    }
+
+    this.click = this.click.bind(this);
+    this.onMouseOver = this.onMouseOver.bind(this);
+    this.onMouseOut = this.onMouseOut.bind(this);
+    this.onMouseOut = this.onMouseOut.bind(this);
   }
 
 
-  click1() {
+  click() {
+    this.setState((estadoAnterio, _props)=> ({
+      numeroClick: estadoAnterio.numeroClick + 1,
+    }))
     console.log(this)
     console.log("Clicou no botão!")
   }
   onMouseOver() {
-    console.log("Passou o mou em cima!")
+    this.setState((estadoAnterior) => ({
+      mouseDentro: estadoAnterior.mouseDentro + 1,
+    }))
+    console.log(this);
   }
   onMouseOut() {
+    this.setState((anterior) => ({
+      mouseFora: anterior.mouseFora + 1,
+    }))
+    console.log(this);
     console.log("Tirou o mouse de cima!")
   }
 
   render() {
     return (
       <div>
-        <button onClick={this.click1} >Botao 1</button>
+        <button onClick={this.click} >Botao 1</button>
         <button onMouseOver={this.onMouseOver} >Botao 2</button>
         <button onMouseOut={this.onMouseOut} >Botao 3</button>
 
